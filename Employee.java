3ebeaa758128e3a8b0c704dd894fe5aa2fd6a31f
UdhayakumarGanesan <31208086+UdhayakumarGanesan@users.noreply.github.com>
2017-08-22 16:23:27 +0530

    give employee count
    counts the members

--- Employee.java
+++ Employee.java
@@ -2,7 +2,7 @@ class Employee{
 public static void main(String args[]){
   for(int i=0;i<5;i++){
 System.out.println("Employee details");
-    System.out.println(i);
+    System.out.println("employee"+i);
   }
 }
 }
