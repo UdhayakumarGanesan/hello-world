d56270feda50899b98e5b9a66d238de7932c745a
UdhayakumarGanesan <31208086+UdhayakumarGanesan@users.noreply.github.com>
2017-08-23 12:55:43 +0530

    add emp_dept value
    adding new emp_dept

--- Employee.java
+++ Employee.java
@@ -3,6 +3,7 @@ public static void main(String args[]){
   for(int i=0;i<5;i++){
 System.out.println("Employee details");
     System.out.println("employee"+i);
+    System.out.println("employee department:");
   }
 }
 }
