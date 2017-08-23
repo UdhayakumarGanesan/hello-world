5016ed35f31c21a1d1dc0bf72f8b0b78cdcb92ed
UdhayakumarGanesan <31208086+UdhayakumarGanesan@users.noreply.github.com>
2017-08-23 13:06:43 +0530

    change to previous one

--- Employee.java
+++ Employee.java
@@ -4,7 +4,6 @@ public static void main(String args[]){
 System.out.println("Employee details");
     System.out.println("employee"+i);
     System.out.println("employee department:");
-   
   }
 }
 }
