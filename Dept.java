2baded561547d1d4204d8e0d5152157162892684
UdhayakumarGanesan <31208086+UdhayakumarGanesan@users.noreply.github.com>
2017-08-28 11:49:52 +0530

    Dev1 (#37)
    * single guideline
    
    want full dept. details
    
    * delete lines
    
    remove lines from the file

--- Dept.java
+++ Dept.java
@@ -1,7 +1,6 @@
 public class Dept{
   //give each and every details in department folder
-  //department information
-  //don't give other info
+
 public static void main(String args[]){
 for(;;)
 {
